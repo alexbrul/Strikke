95ec80fc6133f6f15d92f2461ff16849a2b0c2cd
alexbrul <alexbrul.jobb@gmail.com>
2020-12-08 23:05:33 +0100

    fjernet config filen fra tracking

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBud1D8mGJtOif3IjG6ix1ykxPNsvH8rXU",
-    authDomain: "strikke-8ee10.firebaseapp.com",
-    databaseURL: "https://strikke-8ee10-default-rtdb.firebaseio.com",
-    projectId: "strikke-8ee10",
-    storageBucket: "strikke-8ee10.appspot.com",
-    messagingSenderId: "613440313378",
-    appId: "1:613440313378:web:c81c5abe5c555728eed27b"
-  };
